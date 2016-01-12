7a456f2310ea3a6f11ed875b771153467e6094b6
Jonathan Lima <greenboxal@gmail.com>
2016-01-12 17:22:30 -0200

    FIx and chaining.

--- test/and.js
+++ test/and.js
@@ -15,5 +15,22 @@ describe('and', function() {
 
 		return expect(promise).to.eventually.eql(['123', '456']);
 	});
+
+	it('should not append the result if the previous result was not created by and', function() {
+		var promise = Promise.resolve(['123'])
+		.and(function(value) {
+			expect(value).to.eql(['123']);
+
+			return ['456'];
+		})
+		.and(function(value, value2) {
+			expect(value).to.eql(['123']);
+			expect(value2).to.eql(['456']);
+
+			return 'abc';
+		});
+
+		return expect(promise).to.eventually.eql([['123'], ['456'], 'abc']);
+	});
 });
 
